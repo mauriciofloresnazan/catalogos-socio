75504700905d1d89b4d58fb4c3f9e7a8f3d75739
Alfredo Cano <alfredo.cano@impuls.mx>
2024-05-29 17:50:47 -0600

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MosaicComponent } from './components/mosaic/mosaic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MosaicComponent
   ],
   imports: [
     BrowserModule,
